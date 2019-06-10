a8c3cdfcb4b44f1b6b29ad9d4650c13c2cb06b59
hsrsbd <hsraffety@gmail.com>
2019-06-10 14:09:08 -0700

    refactored to dry

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import MovieCard from './MovieCard';
+
 export default class Movie extends Component {
   constructor(props) {
     super(props);
@@ -46,22 +48,12 @@ export default class Movie extends Component {
     const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className='save-wrapper'>
-        <div className='movie-card'>
-          <h2>{title}</h2>
-          <div className='movie-director'>
-            Director: <em>{director}</em>
-          </div>
-          <div className='movie-metascore'>
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-          {stars.map(star => (
-            <div key={star} className='movie-star'>
-              {star}
-            </div>
-          ))}
-        </div>
-        <div className='save-button'>Save</div>
+        <MovieCard
+          title={this.state.movie.title}
+          director={this.state.movie.director}
+          metascore={this.state.movie.metascore}
+          stars={this.state.movie.stars}
+        />
       </div>
     );
   }
